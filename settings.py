2b94d3e9e0a7cba67b278da34c0553b198e39384
jhan13 <72577544+jhan13@users.noreply.github.com>
2023-03-09 12:02:45 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,15 @@
+# file created by Joon Han 
+#screen dimensions
+
+WIDTH = 800
+HEIGHT = 600
+
+#game settings:
+FPS= 60
+#colors
+BLACK = (0,0,0)
+BLUE = (0,0,225)
+RUNNING = True
+#player attricutes
+PLAYER_ACC = 2
+PLAYER_FRICTION = -0.12
